f2a50e4d7be99d26ebd1454a6b4a25dda83453c2
Seungju Choi <seungjuchoi@Seungjus-MBP-2.lan>
2023-12-06 02:47:20 -0800

    there was updates on the routes for restaurants and search

--- restaurants/routes.js
+++ restaurants/routes.js
@@ -5,15 +5,18 @@ function RestaurantRoutes(app) {
   const createRestaurant = async (req, res) => {
     const restaurant = await dao.createRestaurant(req.body);
       res.json(restaurant);
-    };
-    const deleteRestaurant = async (req, res) => {
-      const status = await dao.deleteRestaurant(req.params.restaurantId);
-      res.json(status);
   };
+
+  const deleteRestaurant = async (req, res) => {
+    const status = await dao.deleteRestaurant(req.params.restaurantId);
+    res.json(status);
+  };
+
   const findRestaurantById = async (req, res) => {
     const restaurant = await dao.findRestaurantById(req.params.restaurantId);
     res.json(restaurant);
   };
+  
   const updateRestaurant = async (req, res) => {
     const { restaurantId } = req.params;
     const status = await dao.updateRestaurant(restaurantId, req.body);
--- search/routes.js
+++ search/routes.js
@@ -1,35 +1,28 @@
 import axios from "axios";
 import express from "express";
 
-export const YELP_API = "https://api.yelp.com/v3/businesses/search";
+export const YELP_API = "https://api.yelp.com/v3/businesses";
 export const YELP_API_KEY = "3sROVy-hI2liNmYmfGPGYd1M4UTKf5WSL6-1meh27jp3Hf95glvX_-4yDEls6sIsQj4tqjCIb0-zF-8TQS1_7RJUyYGl-2gjngWUKkkWzj1fSEldRwnYBYuGqpY-XHYx"
 
 function SearchRoutes(app) {
     const findRestaurants = async (req, res) => {
         const { food } = req.params;
         const { location } = req.params;
-        // const { food, location } = req.query;
-        console.log(food);
-        console.log(location);
+        
         try {
-            console.log("Find Restaurants accessed1");
-            console.log(food);
-            console.log(location);
-
+            console.log("Find Restaurants accessed.");
             const options = {
                 method: 'GET',
-                url: YELP_API,
+                url: `${YELP_API}/search`,
                 params: {location: location, term: food, sort_by: 'best_match', limit: '20'},
                 headers: {
                     accept: 'application/json',
                     Authorization: `Bearer ${YELP_API_KEY}`
                 }
             };
-
             const response = await axios.request(options);
             const restaurants = response.data.businesses;
             console.log(restaurants);
-
             res.json(restaurants);
         } catch (error) {
             console.error(error);
@@ -37,7 +30,29 @@ function SearchRoutes(app) {
         }
     };
 
-    app.get("/api/search/:food/:location", findRestaurants);
+    const findRestaurantById = async (req, res) => {
+        const { restaurantId } = req.params;
+
+        try {
+            const options = {
+                method: 'GET',
+                url: `${YELP_API}/${restaurantId}`,
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${YELP_API_KEY}`
+                }
+            };
+            const response = await axios.request(options);
+            const restaurantDetails = response.data;
+            res.json(restaurantDetails);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    };
+
+    app.get('/api/restaurants/search/:food/:location', findRestaurants);
+    app.get('/api/restaurant/:restaurantId', findRestaurantById);
 }
 
 export default SearchRoutes;
