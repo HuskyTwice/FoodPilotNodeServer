d19b11cd7119ec3140163ede5a664dad251e0e09
Helen Hu <66459726+HelenHuCS@users.noreply.github.com>
2023-12-08 12:11:16 -0800

    initiate the backend for the contact component

---
+++ contact/dao.js
@@ -0,0 +1,5 @@
+// import model from "./model.js";
+// export const createRestaurant = (restaurant) => model.create(contact);
+// export const findRestaurantById = (restaurantId) => model.findById(restaurantId);
+// export const deleteRestaurant = (restaurant) => model.deleteOne(restaurant);
+// export const updateRestaurant = (restaurantId, restaurant) => model.updateOne({ _id: restaurantId }, { $set: restaurant });
